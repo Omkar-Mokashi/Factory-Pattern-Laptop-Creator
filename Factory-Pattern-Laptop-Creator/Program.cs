f0f984770f4c19d03cb5a0d10cbbe513ad8e92bb
Omkar Mokashi <mokashi.omkara@gmail.com>
2021-10-04 19:02:49 +0530

    Add project files.

---
+++ Factory-Pattern-Laptop-Creator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Factory_Pattern_Laptop_Creator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
