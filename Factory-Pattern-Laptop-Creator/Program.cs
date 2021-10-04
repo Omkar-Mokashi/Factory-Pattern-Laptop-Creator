513bc321dc54d1f8712bc769e2a3d86e10fd3edb
Omkar Mokashi <mokashi.omkara@gmail.com>
2021-10-04 21:33:01 +0530

    Added the client code to get the laptop objects

--- Factory-Pattern-Laptop-Creator/Program.cs
+++ Factory-Pattern-Laptop-Creator/Program.cs
@@ -6,7 +6,19 @@ namespace Factory_Pattern_Laptop_Creator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            LaptopFactory laptopFactory = new LaptopFactory();
+
+            ILaptop laptop1 = laptopFactory.GetLaptop("ASUS");
+            Console.WriteLine(laptop1.GetLaptopDetails());
+
+            ILaptop laptop2 = laptopFactory.GetLaptop("MSI");
+            Console.WriteLine(laptop2.GetLaptopDetails());
+
+            ILaptop laptop3 = laptopFactory.GetLaptop("HP");
+            Console.WriteLine(laptop3.GetLaptopDetails());
+
+            ILaptop laptop4 = laptopFactory.GetLaptop("DELL");
+            Console.WriteLine(laptop4.GetLaptopDetails());
         }
     }
 }
